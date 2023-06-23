781e76f49468611dfcbbb4aa8c6f02b2f493e124
Shivansh Raghav <shivanshraghav7@gmai.com>
2023-06-23 15:24:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 //your JS code here. If required.
 function rotate(){
 	const line = document.getElementById("line")
-	line.style.width="200px"
 	line.style.height="2px"
 	line.style.backgroundColor="black"
 	line.style.position="absolute"
