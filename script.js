5f75f4a55a715b28e628d0acf00c49acd591c480
Shivansh Raghav <shivanshraghav7@gmai.com>
2023-06-23 15:21:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
 //your JS code here. If required.
+function rotate(){
+	const line = document.getElementById("line")
+	line.style.width="200px"
+	line.style.height="2px"
+	line.style.backgroundColor="black"
+	line.style.position="absolute"
+	line.style.top="100px"
+	line.style.animation="spin 5s linear infinite"
+	const css = document.styleSheets[0]
+	css.insertRule(`@keyframes spin {
+	0%{
+		transform: rotate(135deg);
+	}
+	20%{
+		transform:rotate(180deg);
+	}
+	40%{
+		transform:rotate(225deg);
+	}
+	60%{
+		transform:rotate(270deg);
+	}
+	80%{
+		transform:rotate(315deg);
+	}
+	100%{
+		transform:rotate(360deg);
+	}`)
+	
+}
